dc5ac53d2ca4bb6fbdc8818f4616bcd291c67540
n0tme <oddostlie@gmail.com>
2020-10-23 02:49:08 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,34 @@
+﻿using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Serialization;
+using n0tFlix.Channel.Pornhub.Configuration;
+using System;
+
+namespace n0tFlix.Channel.Pornhub
+{
+    public class Plugin : BasePlugin<PluginConfiguration>
+    {
+        /// <summary>
+        /// The name of our plugin
+        /// </summary>
+        public override string Name => "Pornhub";
+
+        /// <summary>
+        /// Description of the plugin
+        /// </summary>
+        public override string Description => "Watch pornhub all day long";
+
+        public override Guid Id => Guid.Parse("159e6d66-f87b-4d32-963c-882c29b24308");
+
+        /// <summary>
+        /// Gets the plugin instance.
+        /// </summary>
+        public static Plugin Instance { get; private set; }
+
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+    : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+    }
+}
